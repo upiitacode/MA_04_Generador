d2197c29f0490f832e4c7ac2ff034c26785d4513
Manuel Limones <limonesu.me@gmail.com>
2015-11-13 15:26:09 -0600

    Generate sine wave in timer handle

--- source/main.cc
+++ source/main.cc
@@ -1,7 +1,8 @@
 #include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
 #include "SerialStream_stm32f3.h"
 #include "dac_stm32f3.h"
-#include  <cmath>
+#include "ctimers_stmf3.h"
+#include <cmath>
 
 void tarea1(void const * arguments); //tarea 1
 osThreadId  tarea1ID;	//identificador del hilo tarea 1
@@ -15,14 +16,20 @@ void tarea1Init(void);//funcion que iniciliza la tarea1
 void tarea2Init(void);//funcion que iniciliza la tarea1
 
 int main(){
+	// hardware init
 	dac_init();
+	timer2_init(200, 1);
 	SerialUSART2 serial(9600);
 	serial.printf("\nEl dinero es dinero ara ara\n");
-	//User application
+
+	// Kernel init
 	osKernelInitialize();
 	tarea1Init();
 	tarea2Init();
 	osKernelStart();
+
+	//User application
+	timer2_start();
 	while(1){
 		osDelay(1000);
 	}
@@ -38,11 +45,7 @@ void tarea2Init(void){
 
 void tarea1(void const * arguments){
 	while(1){
-		for(float time = 0.0; time < 3.1416*2; time += 0.1){
-			float volt = (sin(time)+1)*(3.3/2);
-			dac_write(volt);
-			osDelay(1);
-		}
+		osDelay(10);
 	}
 }
 
@@ -50,4 +53,15 @@ void tarea2(void const * arguments){
 	while(1){
 		osDelay(10);
 	}
+}
+
+void timer2_callback(void){
+	static float time = 0;
+	if (time  > (3.1416*2)){
+		time = 0.0;
+	}
+	float volt = (sin(time)+1)*(3.3/2);
+	dac_write(volt);
+	time += 0.01;
+	osDelay(1);
 }
